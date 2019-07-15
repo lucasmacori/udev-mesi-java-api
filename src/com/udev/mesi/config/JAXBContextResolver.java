3e894c879ef47db5b39c163706ea510e90eb1c6c
Lucas <lucasmacori@gmail.com>
2019-07-15 17:33:35 +0200

    (fix) Retour d'une liste même quand il n'y a qu'une seule valeur
    ADD: Implementation de JAXBContextResolver
    UPDATE:
    FIX: Retour JSON
    DELETE:
    
    WARNING:

---
+++ src/com/udev/mesi/config/JAXBContextResolver.java
@@ -0,0 +1,31 @@
+package com.udev.mesi.config;
+
+import com.sun.jersey.api.json.JSONConfiguration;
+import com.sun.jersey.api.json.JSONJAXBContext;
+import com.udev.mesi.messages.*;
+import com.udev.mesi.models.WsFlightDetails;
+
+import javax.ws.rs.ext.ContextResolver;
+import javax.ws.rs.ext.Provider;
+import javax.xml.bind.JAXBContext;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+@Provider
+public class JAXBContextResolver implements ContextResolver<JAXBContext> {
+    private final JAXBContext context;
+    private final Set<Class> types;
+    private Class[] ctypes = {WsResponse.class, WsGetManufacturers.class, WsGetModels.class, WsGetPlanes.class, WsGetFlights.class, WsFlightDetails.class, WsGetPassengers.class, WsGetReservations.class}; //your pojo class
+
+    public JAXBContextResolver() throws Exception {
+        this.types = new HashSet(Arrays.asList(ctypes));
+        this.context = new JSONJAXBContext(JSONConfiguration.natural().build(),
+                ctypes); //json configuration
+    }
+
+    @Override
+    public JAXBContext getContext(Class<?> objectType) {
+        return (types.contains(objectType)) ? context : null;
+    }
+}
