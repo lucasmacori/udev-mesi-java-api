f9e6286f81be11e8629a532dbed48364d29cdad6
Lucas <lucasmacori@gmail.com>
2019-08-23 22:47:42 +0200

    (fix) Reservation -> envoi des détails de vol et du passager
    ADD:
    UPDATE: Entité et model reservations
    FIX:
    DELETE:
    
    WARNING:

--- src/com/udev/mesi/models/WsReservation.java
+++ src/com/udev/mesi/models/WsReservation.java
@@ -11,15 +11,19 @@ public class WsReservation {
     public Long id;
     public Date reservationDate;
     public String reservationClass;
+    public WsFlightDetails flightDetails;
+    public WsPassenger passenger;
     public boolean isActive;
 
     public WsReservation() {
     }
 
-    public WsReservation(Long id, Date reservationDate, String reservationClass, boolean isActive) {
+    public WsReservation(Long id, Date reservationDate, String reservationClass, WsFlightDetails flightDetails, WsPassenger passenger, boolean isActive) {
         this.id = id;
         this.reservationDate = reservationDate;
         this.reservationClass = reservationClass;
+        this.flightDetails = flightDetails;
+        this.passenger = passenger;
         this.isActive = isActive;
     }
 
